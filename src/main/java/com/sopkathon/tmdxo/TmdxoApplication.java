912edd11de36d403a797c130275d48fa838ccdfa
PgmJun <chltmdwns96@gmail.com>
2023-11-25 18:42:35 +0900

    project init

---
+++ src/main/java/com/sopkathon/tmdxo/TmdxoApplication.java
@@ -0,0 +1,13 @@
+package com.sopkathon.tmdxo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmdxoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmdxoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sopkathon/tmdxo/TmdxoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sopkathon.tmdxo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmdxoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
