1ee54c4667b0a4f94f2fc82b5af36d31d3a5f0cf
PgmJun <chltmdwns96@gmail.com>
2023-11-25 23:11:53 +0900

    feat: 내일 일기 전체 조회 로직 구현 #3

--- src/main/java/com/sopkathon/tmdxo/repository/DiaryRepository.java
+++ src/main/java/com/sopkathon/tmdxo/repository/DiaryRepository.java
@@ -1,7 +1,10 @@
 package com.sopkathon.tmdxo.repository;
 
 import com.sopkathon.tmdxo.domain.Diary;
+import java.time.LocalDate;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
+    List<Diary> findByDate(LocalDate date);
 }
