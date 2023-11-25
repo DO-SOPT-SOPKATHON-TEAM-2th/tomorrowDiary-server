12e37e8b80310d667b4bb7716e28b8f9ac60bda2
PgmJun <chltmdwns96@gmail.com>
2023-11-25 23:17:31 +0900

    fix: 정확한 Response 타입 선언 #3

--- src/main/java/com/sopkathon/tmdxo/api/DiaryController.java
+++ src/main/java/com/sopkathon/tmdxo/api/DiaryController.java
@@ -1,5 +1,6 @@
 package com.sopkathon.tmdxo.api;
 
+import com.sopkathon.tmdxo.api.dto.DiaryInfosResponse;
 import com.sopkathon.tmdxo.global.common.response.ApiResponse;
 import com.sopkathon.tmdxo.service.DiaryService;
 import lombok.RequiredArgsConstructor;
@@ -17,7 +18,7 @@ public class DiaryController {
 
     @GetMapping("/diaries")
     @ResponseStatus(HttpStatus.OK)
-    public ApiResponse<?> findAllTomorrowDiaries() {
+    public ApiResponse<DiaryInfosResponse> findAllTomorrowDiaries() {
         return ApiResponse.success(HttpStatus.OK, "내일 일기 전체 조회에 성공하였습니다.", diaryService.findAllTomorrowDiaries());
     }
 }
