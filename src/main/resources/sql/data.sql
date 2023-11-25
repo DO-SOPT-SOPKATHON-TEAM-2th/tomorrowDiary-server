fa47e510f7ee6e2da31345b895fbf02c676a004c
PgmJun <chltmdwns96@gmail.com>
2023-11-26 04:19:55 +0900

    chore: 더미데이터 추가

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO likes(like_count, created_at, modified_at) VALUES (34, '2023-11-27T14:52:55', '2023-11-27T14:52:55');
+INSERT INTO likes(like_count, created_at, modified_at) VALUES (27, '2023-11-27T14:52:55', '2023-11-27T14:52:55');
+INSERT INTO likes(like_count, created_at, modified_at) VALUES (54, '2023-11-27T14:52:55', '2023-11-27T14:52:55');
+
+INSERT INTO diaries(diary_title, email, diary_content, emoji_type, like_id, diary_author, diary_date, created_at, modified_at) VALUES('레드벨벳 케이크와 함께 한 햇살 따뜻한 오후 🍰', 'abc@gmail.com', '준비한 자격증 시험에서 떨어졌지만, 지나간 날을 후회하기 보단 더 공부할 수 있는 시간에 집중하기로 했다.\n\n오늘은 투썸 카페에서 최애 케이크인 레드벨벳 케이크를 아아랑 마시면서 다음 시험 공부 계획을 세웠다. \n\n창문에서 내리쬐는 햇살이 다 괜찮다고 내게 말해주는 것 같았다. 🌻', 'RELIEF', 1, '쿼롱이', '2023-11-27T14:52:55', '2023-11-26T14:52:55', '2023-11-26T14:52:55');
+INSERT INTO diaries(diary_title, email, diary_content, emoji_type, like_id, diary_author, diary_date, created_at, modified_at) VALUES('동네 꽃집에서 작은 선인장 하나를 산 날','abadsc123@gmail.com', '‘나는 내일’ 커뮤니티에서 어떤 분이 칙칙한 사무실 책상에 선인장을 두었더니 기분이 좋아졌다는 글이 있었다.\n\n나도 사무실에서 좋은 감정을 느끼고 싶어 선인장을 샀다.\n\n나를 위로해주는 작은 친구가 생긴 기분이다. 😊', 'CALM', 2, '노란꽃', '2023-11-27T14:52:55', '2023-11-26T14:52:55', '2023-11-26T14:52:55');
+INSERT INTO diaries(diary_title, email, diary_content, emoji_type, like_id, diary_author, diary_date, created_at, modified_at) VALUES('그냥 아무 일도 없이 집에서 넷플릭스 보며 쉬는 날.', 'abqwda123c@gmail.com','요즘 새로 들어간 회사에서 일에 치여 살아 너무 힘들다. 번아웃이 심하게 오는 것 같다.\n\n오늘 하루만 집에서 넷플릭스 보며 잔잔한 하루를 보냈다.\n\n일이 조금 밀린 감이 있지만, 하루 리프레쉬 하는 날이 있어 행복했다. ', 'CALM', 3, '두두', '2023-11-27T14:52:55', '2023-11-26T14:52:55', '2023-11-26T14:52:55');
+
