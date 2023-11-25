304609d083d58cf29a15078e2a492227ba7bb637
PgmJun <chltmdwns96@gmail.com>
2023-11-26 04:02:13 +0900

    fix: EmojiType 구체화 #3

--- src/main/java/com/sopkathon/tmdxo/domain/EmojiType.java
+++ src/main/java/com/sopkathon/tmdxo/domain/EmojiType.java
@@ -1,12 +1,10 @@
 package com.sopkathon.tmdxo.domain;
 
 public enum EmojiType {
-    EMOJI_A,
-    EMOJI_B,
-    EMOJI_C,
-    EMOJI_D,
-    EMOJI_E,
-    EMOJI_F,
-    EMOJI_G,
-    EMOJI_H,
+    ENJOY,
+    MOVING,
+    RELIEF,
+    STABILITY,
+    GREAT,
+    CALM
 }
